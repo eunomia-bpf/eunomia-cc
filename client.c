06a3a355c5eade5d75fbde4fe7a570db7d8eeaa3
yunwei37 <1067852565@qq.com>
2022-08-22 07:36:04 -0700

    update json generator for new proto

--- client.c
+++ client.c
@@ -11,16 +11,17 @@
 #include "cJSON.h"
 #include "base64_encode.h"
 
-const char* config_file_name = "config.json";
+const char *config_file_name = "config.json";
 
-cJSON * parse_json_file(void)
+cJSON *parse_json_file(void)
 {
   FILE *f;
   long len;
   char *data;
 
   f = fopen(config_file_name, "rb");
-  if (!f) {
+  if (!f)
+  {
     fprintf(stderr, "Failed to open config file %s\n", config_file_name);
     exit(1);
   }
@@ -33,6 +34,88 @@ cJSON * parse_json_file(void)
   cJSON *result = cJSON_Parse(data);
   free(data);
   return result;
+}
+
+cJSON *create_map_type(const char *name)
+{
+  if (strcmp(name, "rb") == 0)
+  {
+    cJSON *type = cJSON_CreateString("BPF_MAP_TYPE_RINGBUF");
+    if (type == NULL)
+    {
+      return NULL;
+    }
+    return type;
+  }
+
+  if (strlen(name) > 6 && strncmp(name + strlen(name) - 6, "rodata", 6) == 0)
+  {
+    cJSON *type = cJSON_CreateString("RODATA");
+    if (type == NULL)
+    {
+      return NULL;
+    }
+    return type;
+  }
+  cJSON *type = cJSON_CreateString("BPF_MAP_TYPE_HASH");
+  if (type == NULL)
+  {
+    return NULL;
+  }
+  return type;
+}
+
+cJSON *create_map_json(struct bpf_map_skeleton *bpf_map)
+{
+  if (!bpf_map)
+  {
+    return NULL;
+  }
+  cJSON *map = cJSON_CreateObject();
+  if (map == NULL)
+  {
+    return NULL;
+  }
+  cJSON *name = cJSON_CreateString(bpf_map->name);
+  if (name == NULL)
+  {
+    return NULL;
+  }
+  cJSON_AddItemToObject(map, "name", name);
+  cJSON *type = create_map_type(bpf_map->name);
+  if (type == NULL)
+  {
+    return NULL;
+  }
+  cJSON_AddItemToObject(map, "type", type);
+  return map;
+}
+
+cJSON *create_prog_json(struct bpf_prog_skeleton *bpf_prog)
+{
+  if (!bpf_prog)
+  {
+    return NULL;
+  }
+  cJSON *prog = cJSON_CreateObject();
+  if (prog == NULL)
+  {
+    return NULL;
+  }
+  cJSON *name = cJSON_CreateString(bpf_prog->name);
+  if (name == NULL)
+  {
+    return NULL;
+  }
+  cJSON_AddItemToObject(prog, "name", name);
+
+  cJSON *type = cJSON_CreateString("BPF_PROG_TYPE_TRACEPOINT");
+  if (type == NULL)
+  {
+    return NULL;
+  }
+  cJSON_AddItemToObject(prog, "type", type);
+  return prog;
 }
 
 int main(int argc, char **argv)
@@ -63,7 +146,7 @@ int main(int argc, char **argv)
   cJSON_AddItemToObject(result, "data_sz", data_sz);
 
   base64_buffer = base64_encode(obj.skeleton->data, obj.skeleton->data_sz, &out_len);
-  data = cJSON_CreateString((const char*)base64_buffer);
+  data = cJSON_CreateString((const char *)base64_buffer);
   if (data == NULL)
   {
     goto end;
@@ -73,7 +156,7 @@ int main(int argc, char **argv)
   maps = cJSON_CreateArray();
   for (size_t i = 0; i < obj.skeleton->map_cnt; ++i)
   {
-    map = cJSON_CreateString(obj.skeleton->maps[i].name);
+    map = create_map_json(&obj.skeleton->maps[i]);
     if (map == NULL)
     {
       goto end;
@@ -85,8 +168,8 @@ int main(int argc, char **argv)
   progs = cJSON_CreateArray();
   for (size_t i = 0; i < obj.skeleton->prog_cnt; ++i)
   {
-    prog = cJSON_CreateString(obj.skeleton->progs[i].name);
-    if (prog == NULL)
+    prog = create_prog_json(&obj.skeleton->progs[i]);
+    if (map == NULL)
     {
       goto end;
     }
